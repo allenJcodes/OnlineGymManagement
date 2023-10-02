99c7dd69c9422939f906206d313cff17523449b1
Teyow <61595136+Teyow@users.noreply.github.com>
2023-10-02 23:53:06 +0800

    logout button fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -439,7 +439,7 @@
                                         <form action="{{ route('logout') }}" method="POST">
                                             @csrf
 
-                                            <button type="submit">Logout</button>
+                                            <button type="submit" class="w-full">Logout</button>
                                         </form>
                                     </a>
                                 </li>
