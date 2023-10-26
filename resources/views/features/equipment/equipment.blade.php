325ff53abd31fe6cc3bee914e194045a2c3ca1e6
Teyow <61595136+Teyow@users.noreply.github.com>
2023-10-26 17:08:20 +0800

    fixed add equipment

--- resources/views/features/equipment/equipment.blade.php
+++ resources/views/features/equipment/equipment.blade.php
@@ -12,6 +12,15 @@
                 </a>
             </div>
         @endif
+        @if (Auth::user()->user_role == 2)
+            <div class="flex justify-end pr-4">
+                <a href="{{ route('AddEquipment') }}">
+                    <button type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add
+                        Equipment</button>
+                </a>
+            </div>
+        @endif
 
 
         <div class="grid grid-cols-3">
