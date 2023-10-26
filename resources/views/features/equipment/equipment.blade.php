639989e91ffbaf741156b19d6b06546321d0fcc5
Teyow <61595136+Teyow@users.noreply.github.com>
2023-10-26 16:22:11 +0800

    FIXED EQUIPMENT ADD BUTTON FOR ADMIN

--- resources/views/features/equipment/equipment.blade.php
+++ resources/views/features/equipment/equipment.blade.php
@@ -2,13 +2,16 @@
 
 @section('content')
     <div class="containter pt-12 ">
-        <div class="flex justify-end pr-4">
-            <a href="{{ route('AddEquipment') }}">
-                <button type="button"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add
-                    Equipment</button>
-            </a>
-        </div>
+
+        @if (Auth::user()->user_role == 1)
+            <div class="flex justify-end pr-4">
+                <a href="{{ route('AddEquipment') }}">
+                    <button type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add
+                        Equipment</button>
+                </a>
+            </div>
+        @endif
 
 
         <div class="grid grid-cols-3">
