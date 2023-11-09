079a34e31506694d20faf907660f80698c34bbd8
robbychristian <61650887+robbychristian@users.noreply.github.com>
2023-11-09 15:36:05 +0800

    Updated home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Membership;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $membership = Membership::where('user_id', Auth::user()->id)->first();
+        return view('home', [
+            'membership' => $membership
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,8 +26,26 @@
             <div
                 class="w-full text-center block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Membership Status</h5>
-                <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
-                    acquisitions of 2021 so far, in reverse chronological order.</p>
+                <p class="font-normal text-gray-700 dark:text-gray-400">
+                    @if ($membership->date_ended === null)
+                        <div class="flex justify-center items-center">
+                            <span class="h-2.5 w-2.5 rounded-full bg-red-500 mr-2">
+                            </span>
+                            <span class="text-2xl">
+                                No Subscription Yet
+                            </span>
+                        </div>
+                    @else
+                        <div class="flex justify-center items-center">
+                            <span class="h-2.5 w-2.5 rounded-full bg-green-500 mr-2">
+                            </span>
+                            <span class="text-2xl">
+                                {{ $membership->date_ended }}
+                            </span>
+
+                        </div>
+                    @endif
+                </p>
             </div>
         </div>
     </div>
