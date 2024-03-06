52f4d5f238684f7d5f2d833d93e8fd8b3c7e38f3
michaelaaquino <michaela.aquino@redcoresolutions.com>
2024-03-06 23:35:04 +0800

    fix sidebar focus bug

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -156,7 +156,7 @@
                 </li>
                 <li>
                     <a href="{{ route('attendance') }}"
-                        class="{{ Route::is('users', 'editUsers', 'addUsers') ? 'bg-blue-900 text-white' : '' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-400 dark:hover:bg-gray-700 group">
+                        class="{{ Route::is('attendance', 'editAttendance', 'addUsers') ? 'bg-blue-900 text-white' : '' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-400 dark:hover:bg-gray-700 group">
                         <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                             viewBox="0 0 20 18">
