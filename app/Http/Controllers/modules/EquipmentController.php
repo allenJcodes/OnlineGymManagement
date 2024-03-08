d99b041536b77ab0658bce044cc03efd434d0dfd
Miguel <miguelarciagaa@gmail.com>
2024-03-08 23:23:03 +0800

    equipment error fix

--- app/Http/Controllers/modules/EquipmentController.php
+++ app/Http/Controllers/modules/EquipmentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\modules;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Equipment\EquipmentStoreRequest;
 use App\Http\Requests\Equipment\EquipmentUpdateRequest;
+use App\Models\Equipment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\EquipmentType;
