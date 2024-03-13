b6457d0db2232c3ced80ac8716580f95a8494ce7
allenJcodes <110220645+allenJcodes@users.noreply.github.com>
2024-03-14 03:52:59 +0800

    login form update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -24,81 +25,46 @@
 </style>
 
 <body>
-
-
-    <div class="flex justify-center pt-32">
-
-        <div
-            class="block w-2/6 max-w-6xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <a href="/" class="p-2 bg-red-600 rounded-md text-white hover:bg-slate-300">Home</a>
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="flex justify-center">
-                            <img src="{{ asset('images/logo.png') }}" alt="" height="90" width="90">
-                        </div>
-                        <div class="text-center text-4xl pb-5 pt-5">Japs Fitness Gym</div>
-                        <br>
-                        <div class="card-body">
-                            <form method="POST" action="{{ route('login') }}">
-                                @csrf
-                                <div class="relative">
-                                    <input type="text" id="floating_outlined"
-                                        class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                        name="email" value="{{ old('email') }}" required autocomplete="email"
-                                        placeholder=" " autofocus />
-                                    <label for="floating_outlined"
-                                        class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
-                                        Email</label>
-                                </div>
-                                <br>
-                                <div class="relative">
-                                    <input type="password" id="floating_outlined"
-                                        class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                        name="password" required autocomplete="current-password" placeholder=" ">
-                                    <label for="floating_outlined"
-                                        class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
-                                        Password</label>
-                                </div>
-                                <div class="flex items-start mb-6 pt-2">
-                                    <div class="flex items-center h-5">
-                                        <input id="remember" type="checkbox" value="" name="remember"
-                                            id="remember" {{ old('remember') ? 'checked' : '' }}
-                                            class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800">
-                                    </div>
-                                    <label for="remember"
-                                        class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember
-                                        me</label>
-                                </div>
-                                <div class="flex justify-center">
-                                    <button type="submit"
-                                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
-
-
-                                </div>
-                                <div class="flex justify-center pt-2">
-                                    @if (Route::has('password.request'))
-                                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                                            <span class="hover:text-gray-400">Forget your password?</span>
-                                        </a>
-                                    @endif
-                                </div>
-                                <div class="flex justify-center pt-2">
-                                    <a class="btn btn-link" href="{{ route('register') }}">
-                                        <span class="hover:text-gray-400 text-center">
-                                            Register
-                                            here!</span>
-                                    </a>
-                                </div>
-                            </form>
-
-                        </div>
-                    </div>
-                </div>
+    <div class="flex h-screen justify-center font-red-hat-display md:px-0 py-32">
+        <div class="flex flex-col h-fit bg-white rounded-lg p-6 md:w-1/2 xl:w-1/3 gap-10">
+            <div class="flex justify-center w-full">
+                <img class="" src="{{ asset('images/logo.png') }}" alt="" height="120" width="120">
             </div>
+            <br>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="form-field-container">
+                    <label for="email" class="form-label">Email</label>
+                    <input id="email" type="text" name="email" class="form-input rounded-lg" value="{{ old('email')}}" placeholder="" autocomplete="email" required>
+                </div>
+                <div class="form-field-container mt-2">
+                    <label for="password" class="form-label">Password</label>
+                    <input id="password" type="password" name="password" class="form-input rounded-lg" value="{{ old('email')}}" placeholder="" autocomplete="current-password" required>
+                </div>
+                <div class="flex justify-between items-center mt-2 mb-6">
+                    <div class="flex items-start">
+                        <div class="flex items-center h-5">
+                            <input id="remember" type="checkbox" value="" name="remember" {{ old('remember') ? 'checked' : '' }} class="w-4 h-4 border border-gray-600 rounded">
+                        </div>
+                        <label for="remember" class="ml-2 text-sm text-background/80">Remember me</label>
+                    </div>
+                    <a class="text-sm text-background/80 hover:text-gray-400" href="{{ route('password.request') }}">
+                        Forget your password?
+                    </a>
+                </div>
+                <div class="flex justify-center">
+                    <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Login</button>
+
+
+                </div>
+                <div class="flex justify-center pt-2">
+                    <a class="btn btn-link text-background/80 hover:text-gray-400 text-center" href="{{ route('register') }}">
+                        Register here!
+                    </a>
+                </div>
+            </form>
         </div>
     </div>
-
 </body>
 
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,58 +20,50 @@
 
 <body class="font-red-hat-display bg-background">
 
-    <nav class="bg-background fixed w-full z-20 top-0 left-0 h-16 flex items-center justify-center px-64">
-        <div class="w-full flex items-center justify-between">
+    <nav class="bg-background fixed w-full z-20 top-0 left-0 py-2 h-16 flex items-center justify-center xl:px-64">
+        <div class="w-full flex items-center justify-between px-16">
             <a href="/" class="flex items-center">
-
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"><img
-                        src="{{ asset('images/logo.png') }}" alt="" width="50" height="50"></span>
+                <img src="{{ asset('images/logo.png') }}" alt="" width="50" height="50">
             </a>
-            <div class="flex md:order-2 items-center">
+            <div class="flex items-center">
                 @if (Route::has('login'))
-                    <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-                        id="navbar-sticky">
-                        <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
+                        <ul class="flex font-medium items-center">
                             <li>
-                                <a href="#session" class="block py-2 pl-3 pr-4 rounded-md text-white hover:text-yellow-300" aria-current="page">
+                                <a href="#session" class="block py-2 px-4 rounded-md text-white hover:text-yellow-300" aria-current="page">
                                     Session
                                 </a>
                             </li>
                             <li>
-                                <a href="#pricing" class="block py-2 pl-3 pr-4 rounded-md text-white hover:text-yellow-300" aria-current="page">
+                                <a href="#pricing" class="block py-2 px-4 rounded-md text-white hover:text-yellow-300" aria-current="page">
                                     Pricing
                                 </a>
                             </li>
                             <li>
-                                <a href="#learn" class="block py-2 pl-3 pr-4 rounded-md text-white hover:text-yellow-300" aria-current="page">
+                                <a href="#learn" class="block py-2 px-4 rounded-md text-white hover:text-yellow-300" aria-current="page">
                                     Learn
                                 </a>
                             </li>
                             <li>
-                                <a href="#faq" class="block py-2 pl-3 pr-4 rounded-md text-white hover:text-yellow-300" aria-current="page">
+                                <a href="#faq" class="block py-2 px-4 rounded-md text-white hover:text-yellow-300" aria-current="page">
                                     FAQ
                                 </a>
                             </li>
                             <li>
-                                <a href="#instructor" class="block py-2 pl-3 pr-4 rounded-md text-white hover:text-yellow-300" aria-current="page">
+                                <a href="#instructor" class="block py-2 px-4 rounded-md text-white hover:text-yellow-300" aria-current="page">
                                     Instructors
                                 </a>
                             </li>
                             <li>
-                                <a href="#contact" class="block py-2 pl-3 pr-4 rounded-md text-white hover:text-yellow-300" aria-current="page">
+                                <a href="#contact" class="block py-2 px-4 rounded-md text-white hover:text-yellow-300" aria-current="page">
                                     Contact
                                 </a>
                             </li>
                             <li>
-                                <a href="{{ route('login') }}"
-                                    class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"><span
-                                        class="text-black bg-yellow-400 p-2 rounded-xl hover:bg-gray-400">{{auth()->check() ? 'My Dashboard' : 'Login'}} </span></a>
+                                <a href="{{ route('login') }}" class="block py-2 px-4 text-black bg-yellow-400 p-2 rounded-xl hover:bg-gray-400 whitespace-nowrap">
+                                    {{auth()->check() ? 'My Dashboard' : 'Login'}}
+                                </a>
                             </li>
-
-
-
                         </ul>
-                    </div>
                 @endif
                 <button data-collapse-toggle="navbar-sticky" type="button"
                     class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -89,7 +81,7 @@
     </nav>
 
     {{-- HERO --}}
-    <div class="flex items-center px-64 gap-32 pt-16 pb-0 h-[90vh] relative">
+    <div class="flex items-center gap-32 pt-16 pb-0 h-[90vh] relative px-16 xl:px-64">
         <div class="z-10 w-3/4" >
             <h1 class="text-6xl pb-5 text-white font-bold"> Become <span class="text-yellow-300">Physically Fit</span></h1>
 
@@ -110,7 +102,7 @@
 
     {{-- SESSION PAGE --}}
     <section id="session">
-        <div class="py-20 px-64 gap-10 bg-off-white flex flex-col items-center justify-center">
+        <div class="py-20 gap-10 bg-off-white flex flex-col items-center justify-center px-16 xl:px-64 ">
 
             <h2 class="text-5xl text-background">Gym Sessions</h2>
 
@@ -138,7 +130,7 @@
 
     {{-- LEARN PAGE --}}
     <section id="learn">
-        <div class="py-20 px-64 gap-8 bg-off-white flex flex-col items-center justify-center">
+        <div class="py-20 gap-8 bg-off-white flex flex-col items-center justify-center px-16 xl:px-64 ">
             @foreach ($learnContent as $index => $learn_content)
 
                 <x-learn-content 
@@ -155,7 +147,7 @@
 
     {{-- PRICING PAGE --}}
     <section id="pricing">
-        <div class="py-20 px-64 gap-10 bg-background flex flex-col items-center justify-center text-off-white">
+        <div class="py-20 gap-10 bg-background flex flex-col items-center justify-center text-off-white px-16 xl:px-64">
             <h2 class="text-5xl">Membership Pricing</h2>
 
             <div class="grid grid-cols-3 gap-10 w-full">
@@ -176,7 +168,7 @@
 
     {{-- INSTRUCTOR --}}
     <section id="instructor">
-        <div class="py-20 px-64 gap-10 bg-off-white flex flex-col items-center justify-center">
+        <div class="py-20 gap-10 bg-off-white flex flex-col items-center justify-center px-16 xl:px-64">
 
             <h2 class="text-5xl text-background">Our Instructors</h2>
 
@@ -196,7 +188,7 @@
 
     {{-- FAQ --}}
     <section id="faq">
-        <div class="py-20 px-64 gap-10 bg-off-white flex flex-col items-center justify-center">
+        <div class="py-20 gap-10 bg-off-white flex flex-col items-center justify-center px-16 xl:px-64">
             <h2 class="text-5xl text-background">Frequently Asked Questions</h2>
 
             <div class="w-full" id="accordion-collapse" data-accordion="collapse">
@@ -234,7 +226,7 @@
 
     {{-- CONTACT PAGE --}}
     <section id="contact">
-        <div class="pl-36 pr-36 pb-20 pt-20 bg-off-white">
+        <div class="py-20 bg-off-white px-16 xl:px-64">
             <div class="grid grid-cols-2">
                 <div class="col-span-1">
                     <legend class="text-center text-4xl pb-10">Contact Information</legend>
@@ -251,7 +243,7 @@
 
                 </div>
                 <div class="col-span-1">
-                    <iframe src="https://www.google.com/maps?q={{ $coordinates->content }}&z=18&output=embed"
+                    <iframe class="max-w-full" src="https://www.google.com/maps?q={{ $coordinates->content }}&z=18&output=embed"
                         width="600" height="300" style="border:0; padding-left: 2rem" allowfullscreen=""
                         loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
