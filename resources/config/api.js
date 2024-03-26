1c0f0696c95208b67a8e1c6c6a84e88293a1236a
Miguel Arciaga <miguelarciagaa@gmail.com>
2024-03-27 01:03:00 +0800

    api

--- resources/config/api.js
+++ resources/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const DEV_URL = "http://127.0.0.1:8000/";
-export const PROD_URL = "https://www.japsgym.online/";
+export const PROD_URL = "https://japsgym.online/";
 
 
 export const api = axios.create({
