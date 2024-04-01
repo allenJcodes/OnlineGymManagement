554e5770341a9394a7fb9047c370b4d9f71fbeed
Miguel Arciaga <miguelarciagaa@gmail.com>
2024-04-02 01:18:12 +0800

    public path save file fix

--- app/Services/MembershipService.php
+++ app/Services/MembershipService.php
@@ -31,11 +31,11 @@ class MembershipService {
         ->validateResult(false)
         ->build();
 
-        $filePath = '/images/qr/' . time() . '_' . request()->user_id . "_qr_code.png";
+        $filePath = 'images/qr/' . time() . '_' . request()->user_id . "_qr_code.png";
 
         checkAndCreatePublicDir('images/qr/');
 
-        $result->saveToFile($_SERVER['DOCUMENT_ROOT'] . $filePath);
+        $result->saveToFile(public_path('/') . $filePath);
 
         return $filePath;
     }
