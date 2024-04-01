96ec427047915b413f42b4cbfef33a64b9021b0d
Miguel Arciaga <miguelarciagaa@gmail.com>
2024-04-02 01:39:36 +0800

    payment reports fix

--- resources/views/features/payment/Report.blade.php
+++ resources/views/features/payment/Report.blade.php
@@ -63,47 +63,6 @@
                     <td class="py-2">
                         {{ $payment->status }}
                     </td>
-                    @if (Auth::user()->user_role == '1')
-                        <td class="py-2">
-                            <div class="flex items-center w-full">
-                                <div class="text-left">
-                                    <button id="dropdownButton" data-dropdown-toggle="toggle{{ $payment->id }}"
-                                        class="" type="button">
-                                        <span class="sr-only">Open dropdown</span>
-                                        <svg class="h-4 w-4" aria-hidden="true"
-                                            xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                            viewBox="0 0 16 3">
-                                            <path
-                                                d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
-                                        </svg>
-                                    </button>
-                                </div>
-
-                                <div id="toggle{{ $payment->id }}"
-                                    class="z-10 hidden bg-white border border-light-gray-background text-background rounded-md !min-w-[8vw]">
-
-                                    <div class="flex flex-col gap-2 divide-y divide-light-gray-background">
-
-                                        <p class="text-background/70 text-sm pt-2 px-4 text-center">Actions</p>
-
-                                        <div class="flex flex-col divide-y divide-light-gray-background"
-                                            aria-labelledby="dropdownButton">
-
-                                            <button data-modal-target="popup-modal"
-                                                data-modal-toggle="popup-modal"
-                                                class="py-2 px-4 hover:bg-off-white transition-all edit-button
-                                                "
-                                                data-payment="{{ json_encode($payment) }}"
-                                                @if ($payment->status == 'Paid') disabled @endif>
-                                                Edit
-                                            </button>
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </td>
-                    @endif
                 </tr>
                 @empty
                 <tr>
