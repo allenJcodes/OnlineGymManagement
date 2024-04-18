245855941bcedf8f0ebf53f1582d549a8af76aa9
Miguel <miguelarciagaa@gmail.com>
2024-04-18 21:28:44 +0800

    notif count

--- resources/views/components/user-notification.blade.php
+++ resources/views/components/user-notification.blade.php
@@ -1,17 +1,46 @@
-<div class="flex flex-col">
-    @forelse ($userNotifications as $userNotification)
-        <div class="flex flex-col p-2">
-            <p class="text-xs text-dark-gray-800">{{$userNotification->notification->type}}</p>
-            <p>{{$userNotification->notification->content}}</p>
-            <p class="text-xs">{{$userNotification->notification->created_at->format('h:i')}}</p>
-        </div>
-    @empty
-        <p>There are no new notifications.</p>
-    @endforelse
+<button type="button" data-dropdown-toggle="notification-dropdown"
+    class="p-2 mr-1 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600">
+    <span class="sr-only">View notifications</span>
+    <!-- Bell icon -->
 
-    
-    
-    @if(count($userNotifications))
-        <a href="{{route('clearNotifications')}}" class="outline-button text-center">Clear Notifications</a>
-    @endif
+    <div class="relative">
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 20">
+            <path
+                d="M12.133 10.632v-1.8A5.406 5.406 0 0 0 7.979 3.57.946.946 0 0 0 8 3.464V1.1a1 1 0 0 0-2 0v2.364a.946.946 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C1.867 13.018 0 13.614 0 14.807 0 15.4 0 16 .538 16h12.924C14 16 14 15.4 14 14.807c0-1.193-1.867-1.789-1.867-4.175ZM3.823 17a3.453 3.453 0 0 0 6.354 0H3.823Z" />
+        </svg>
+
+        @if ($userNotifications->count())
+            <p class="absolute  bottom-[45%] left-[45%] flex items-center justify-center text-white text-xs rounded-full size-5 bg-red-500">
+                {{$userNotifications->count()}}
+            </p>
+        @endif
+
+    </div>
+</button>
+
+<!-- Dropdown menu -->
+<div class="hidden overflow-hidden bg-white rounded-b-md shadow-md max-w-[20vw]" id="notification-dropdown">
+    <div class="flex flex-col p-2">
+        <p class="text-base font-medium text-left w-full">
+            Notifications
+        </p>
+
+        <div class="flex flex-col">
+            @forelse ($userNotifications as $userNotification)
+                <div class="flex flex-col p-2">
+                    <p class="text-xs text-dark-gray-800">{{ $userNotification->notification->type }}</p>
+                    <p>{{ $userNotification->notification->content }}</p>
+                    <p class="text-xs">{{ $userNotification->notification->created_at->format('h:i') }}</p>
+                </div>
+            @empty
+                <p>There are no new notifications.</p>
+            @endforelse
+
+
+
+            @if (count($userNotifications))
+                <a href="{{ route('clearNotifications') }}" class="outline-button text-center">Clear Notifications</a>
+            @endif
+        </div>
+    </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,27 +80,7 @@
                         <span id="datetime" class="text-sm mx-2 font-semibold whitespace-nowrap text-white"></span>
 
                         <!-- Notifications -->
-                        <button type="button" data-dropdown-toggle="notification-dropdown"
-                            class="p-2 mr-1 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600">
-                            <span class="sr-only">View notifications</span>
-                            <!-- Bell icon -->
-                            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                fill="currentColor" viewBox="0 0 14 20">
-                                <path
-                                    d="M12.133 10.632v-1.8A5.406 5.406 0 0 0 7.979 3.57.946.946 0 0 0 8 3.464V1.1a1 1 0 0 0-2 0v2.364a.946.946 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C1.867 13.018 0 13.614 0 14.807 0 15.4 0 16 .538 16h12.924C14 16 14 15.4 14 14.807c0-1.193-1.867-1.789-1.867-4.175ZM3.823 17a3.453 3.453 0 0 0 6.354 0H3.823Z" />
-                            </svg>
-                        </button>
-
-                        <!-- Dropdown menu -->
-                        <div class="hidden overflow-hidden bg-white rounded-b-md shadow-md max-w-[20vw]" id="notification-dropdown">
-                            <div class="flex flex-col p-2">
-                                <p class="text-base font-medium text-left w-full">
-                                    Notifications
-                                </p>
-    
-                                <x-user-notification/>
-                            </div>
-                        </div>
+                        <x-user-notification/>
 
                         <button type="button" class="flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="dropdowns">
                             <span class="sr-only">Open user menu</span>
